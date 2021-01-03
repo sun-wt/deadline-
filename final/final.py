8f159f98b0bf78cae7e959d78c810422d5276de1
anci <anci.peng@gmail.com>
2021-01-03 18:51:52 +0800

    替換單詞的問題已解決，看是否需要再增加替換的單詞，接下來要再需備註的專有名詞旁加上註解

--- final/final.py
+++ final/final.py
@@ -8,13 +8,26 @@ def jsonReader(File):
     with open(File,'r',encoding='utf-8') as f:
         result = json.load(f)
     return result
+def repl(target,replaceLIST,replacements):
+    for i in range(len(target)):
+        if target[i] in replaceLIST and target[i-1]=='[':
+            continue
+        elif target[i] in replaceLIST:
+            n=replaceLIST.index(target[i])
+            target=target.replace(target[i],replacements[n])
+            i+=2
+    return target
+
 if __name__ == "__main__":
     txt = txtReader('../民法.txt')
     txt=txt.replace('\n','')
     # 先將文本中的換行去掉，因為法律條文並沒有用自動排版，而是手動換行
-
+    replaceLIST=jsonReader('./replace.json')["Replace"]
+    replacementsLIST=jsonReader('./replace.json')["replacements"]
+    txt=repl(txt,replaceLIST,replacementsLIST)
+    
     # json=jsonReader('./replace.json')["replace"].replace_in_list(jsonReader('./replace.json')["replace"],jsonReader('./replace.json')["replacements"])
     # txtRaplace=txt.replace(txt, json["replacements"])
     #這邊是想要將DICT中的詞替換掉，不過還沒找到方法，上面是失敗的例子XD
 
-    # print(json)
+    print(txt)
